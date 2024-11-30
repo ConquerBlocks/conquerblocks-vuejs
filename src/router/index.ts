1786d18fcdba3d8d88ea8be67ce8ee80e255b296
alan buscaglia <alanbuscaglia@gmail.com>
2024-11-30 18:47:02 +0100

    feat: clase 3 - interceptores y enrutamiento

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,33 @@
+import LoginPage from '@/views/Login/LoginPage.vue'
+import CharactersPage from '@/views/Private/Characters/CharactersPage.vue'
+import RegisterPage from '@/views/Register/RegisterPage.vue'
+
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component:
-    // },
+    { path: '/', redirect: { name: 'Login' } },
+    { path: '/login', name: 'Login', component: LoginPage },
+    { path: '/register', name: 'Register', component: RegisterPage },
+    { path: '/characters', name: 'CharactersContainer', component: CharactersPage },
   ],
+})
+
+router.beforeEach(async (to, _from, next) => {
+  const publiPages = ['Login', 'Register']
+  const authRequired = !publiPages.includes(to.name as string)
+
+  const token = localStorage.getItem('token')
+
+  if (authRequired) {
+    // TODO: iniciar store
+    if (!token) {
+      return next({ name: 'Login' })
+    }
+  }
+
+  next()
 })
 
 export default router
---
+++ src/views/Private/Characters/CharactersPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Personajes de mi lista</h1>
+    <!-- aca iria la lista de personajes -->
+  </div>
+</template>
+<script lang="ts" setup>
+console.log('Charactters')
+// TODO: aca tenemos que traer los datos de los personajes
+</script>
+<style lang="scss"></style>
