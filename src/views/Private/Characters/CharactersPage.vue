7a925b76e9698ecadd14a17ed81a3877ecc8a019
alan buscaglia <alanbuscaglia@gmail.com>
2024-12-01 14:58:54 +0100

    feat: clase 4 -stores

--- src/views/Private/Characters/CharactersPage.vue
+++ src/views/Private/Characters/CharactersPage.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <h1>Personajes de mi lista</h1>
-    <!-- aca iria la lista de personajes -->
+    <CharactersList :characters="characters" />
   </div>
 </template>
 <script lang="ts" setup>
-console.log('Charactters')
+import { useCharacterStore } from '@/stores/useCharacterStore'
+import { computed } from 'vue'
+import CharactersList from './CharactersList/CharactersList.vue'
+
+console.log('Characters')
+const characterStore = useCharacterStore()
+
+const characters = computed(() => characterStore.formattedCharacters)
 // TODO: aca tenemos que traer los datos de los personajes
 </script>
 <style lang="scss"></style>
